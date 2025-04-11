912ecd06784fec02bb1b930ee95da57291be96e3
Diego Cruz <dactjs@icloud.com>
2025-04-11 09:22:16 -0400

    chore: add docker

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,14 @@ import path from "node:path";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
 });
